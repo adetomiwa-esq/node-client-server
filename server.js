b2e4fed447450e8f95d4bfb62ecae74a39a73b3f
Akinola Kehinde <khennyakin@gmail.com>
2023-07-07 19:37:29 +0100

    server

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
 const http = require('http');
-const { localhost } = require('viem/chains');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     console.log('request made')
     res.setHeader('Content-Type', 'text/html')
 
-    res.write('<h1>hello server</h1>')
-    res.write('<h1>hello again, server</h1>')
-    res.end()
+    // res.write('<h1>hello server</h1>')
+    // res.write('<h1>hello again, server</h1>')
+    // res.end()
+    let path = './views/';
+
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about')
+            res.end()
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    fs.readFile(path, (err, data) => {
+        if(err){
+            console.log(err)
+            res.end()
+        }
+        else {
+            // res.write(data)
+            res.end(data)
+        }
+    })
 })
 
 server.listen(3000, 'localhost', () => {
